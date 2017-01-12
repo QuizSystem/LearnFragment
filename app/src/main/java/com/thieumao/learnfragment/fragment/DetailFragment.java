10a7a46480ddf0f21fd10310ba5bfbbf92f706b4
Nguyen Van Thieu B <nguyen.van.thieub@framgias-Mac-mini-6.local>
2017-01-12 20:56:05 +0700

    Learn Fragment

---
+++ app/src/main/java/com/thieumao/learnfragment/fragment/DetailFragment.java
@@ -0,0 +1,23 @@
+package com.thieumao.learnfragment.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.thieumao.learnfragment.R;
+
+/**
+ * Created by nguyen.van.thieub on 1/12/17.
+ */
+
+public class DetailFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.fragment_detail, container, true);
+        return fragmentView;
+    }
+}
